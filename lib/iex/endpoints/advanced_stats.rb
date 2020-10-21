f8bf73ee6fa5d6c8de9c1f644b729af7d71f6601
Alessio Fanelli <fana@alessiofanelli.com>
2020-10-20 20:12:58 -0400

    Stash

--- lib/iex/endpoints/advanced_stats.rb
+++ lib/iex/endpoints/advanced_stats.rb
@@ -2,7 +2,7 @@ module IEX
   module Endpoints
     module AdvancedStats
       def advanced_stats(symbol, options = {})
-        IEX::Resources::AdvancedStats.new(get("stock/#{symbol}/advanced-stats", options))
+        IEX::Resources::AdvancedStats.new(get("stock/#{symbol}/advanced-stats", { token: publishable_token }.merge(options)))
       rescue Faraday::ResourceNotFound => e
         raise IEX::Errors::SymbolNotFoundError.new(symbol, e.response[:body])
       end
